dc12eea591714368a441e772a28f417a1557b202
minhtuanchannhan <minhtuanchannhan@gmail.com>
2020-03-11 16:23:47 +0700

    Retreive places list from Google Maps

--- src/controllers/place.controller.js
+++ src/controllers/place.controller.js
@@ -8,11 +8,31 @@ const googleMapsClient = require('@google/maps').createClient({
 });
 
 exports.list = async (req, res, next) => {
+	let lat = req.body.lat || '';
+	let lng = req.body.lng || '';
+
 	try {
-		res.json({
-			message: 'Successful.',
-			status: httpStatus.OK,
-		});
+		if (lat && lng) {
+			let places = googleMapsClient.placesNearby({
+				location: { lat, lng },
+				radius: req.body.radius || 50000,
+			}).asPromise().then(response => {
+				res.json({
+					code: 200,
+					message: 'Successful.',
+					data: response.json.results
+				});
+			}).catch((err) => {
+				err.message = 'Places was not found.'
+				next(err);
+			});
+		} else {
+			res.status(httpStatus.UNPROCESSABLE_ENTITY);
+			res.json({
+				message: 'Validate Error.',
+				errors: 'lat and lng are required fields.'
+			});
+		}
 	} catch (err) {
 		next(err);
 	}
